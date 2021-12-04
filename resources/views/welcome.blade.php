15caaf7b52ddbfad2de822896f9ac2b432dbd27e
Andrei Lepekhov <lepekhov.andrei@gmail.com>
2021-12-04 16:08:27 +0200

    welcome title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    2KN
                 </div>
 
                 <div class="links">
