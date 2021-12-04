38349339a87c5365efe4dd1411ff7dc68df8c1d1
Andrei Lepekhov <lepekhov.andrei@gmail.com>
2021-12-04 16:06:28 +0200

    meta

--- resources/views/loyouts/main.blade.php
+++ resources/views/loyouts/main.blade.php
@@ -4,6 +4,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="description" content="">
+  <meta name="keywords" content="">
   <link rel="shortcut icon" href="{{ '/img/favicon.ico' }}" type="image/x-icon" />
   <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;700&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="{{ asset('css/app.css') }}">
